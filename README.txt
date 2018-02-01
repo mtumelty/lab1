4896aed8cfc26212734d70fc92e3ebac622063b7
mtumelty <m186606@usna.edu>
2018-01-31 21:11:48 -0500

    Finalized code :)

--- README.txt
+++ README.txt
@@ -3,7 +3,12 @@ I am still working on...
 1) Creating the color screen to reflect the color of my LED
 	-stuck with syntax and understanding of how to get window to change colors
 
-31JAN
+31JAN 2000
 1) Code works with following exceptions:
 -Green on LED light is not reflected on LED Mirror
 -Have to attempt to run the code multiple times before it works due to faulty data causing it to crash on startup
+
+31JAN 2115
+Code works completely, solved above solutions by...
+1) adding a try-except statement for both index and value errors
+2) add QUIT feature so you can just exit out of the LED Mirror and the program will stop running
--- rgb.py
+++ rgb.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
 import serial
-import pygame, sys
+import pygame
+from pygame.locals import *
+import sys
 import time
 
 s = serial.Serial("/dev/ttyACM0")
+l = 0
 pygame.init()
 
 DISPLAYSURF = pygame.display.set_mode((400, 300))
@@ -12,15 +15,24 @@ pygame.display.set_caption('LED mirror')
 
 while(True):
 	l = s.readline()
-	COLOR = l.rstrip().split(", ")
-	COLOR_R = int(COLOR[0])
-	COLOR_G = int(COLOR[1])
-	COLOR_B = int(COLOR[2])
-	print COLOR
-	DISPLAYSURF.fill((COLOR_R, COLOR_G, COLOR_B))
+	COLOR = l.rstrip().split(",")
+	try:
+		try:
+			COLOR_R = int(COLOR[0])
+			COLOR_G = int(COLOR[1])
+			COLOR_B = int(COLOR[2])
+			print COLOR
+			DISPLAYSURF.fill((COLOR_R, COLOR_G, COLOR_B))
 	
-	'''for event in pygame.event.get():
+	
+			pygame.display.update()
+		except IndexError:
+			print('IndexPASS')
+
+	except ValueError:
+		print("ValuePASS")
+		
+	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
-			sys.exit()'''
-	pygame.display.update()
+			sys.exit()
