5e98aa3f34e4d55d67e185ebe2259b0b30a36d11
mtumelty <m186606@usna.edu>
2018-01-31 19:15:32 -0500

    Code works

--- rgb.py
+++ rgb.py
@@ -2,6 +2,7 @@
 
 import serial
 import pygame, sys
+import time
 
 s = serial.Serial("/dev/ttyACM0")
 pygame.init()
@@ -11,7 +12,12 @@ pygame.display.set_caption('LED mirror')
 
 while(True):
 	l = s.readline()
-	print l.rstrip().split(",")
+	COLOR = l.rstrip().split(", ")
+	COLOR_R = int(COLOR[0])
+	COLOR_G = int(COLOR[1])
+	COLOR_B = int(COLOR[2])
+	print COLOR
+	DISPLAYSURF.fill((COLOR_R, COLOR_G, COLOR_B))
 	
 	'''for event in pygame.event.get():
 		if event.type == QUIT:
